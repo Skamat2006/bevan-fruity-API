76c93245ff3ba886e455336a4a430d5e047a50de
Skamat2006 <sonalparab2006@gmail.com>
2025-01-21 09:57:37 +0000

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const cors = require("cors")
 const app = express()
 
+const logger = require("./logger")
 
 // Middleware
+app.use(cors())
 app.use(express.json())
+app.use(logger)
 
 const fruitsRouter = require("./routes/fruits")
 
